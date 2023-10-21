cac80513eeb03983109789b524b47152809a96bf
sharonasaja <sharon.asaja@student.moringaschool>
2023-10-22 00:20:58 +0300

    attempted lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,36 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+         try:
+            response = requests.get(self.url)
+            response.raise_for_status()  # Raise an exception for HTTP errors (e.g., 404, 500)
+            return response.text
+         except requests.exceptions.RequestException as e:
+            # Handle any request exceptions (e.g., connection error, timeout)
+          print(f"An error occurred: {e}")
+          return None
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body is not None:
+            try:
+                data = json.loads(response_body)
+                return data
+            except json.JSONDecodeError as e:
+                print(f"Failed to decode JSON: {e}")
+                return None
+        else:
+            return None
+
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+requester = GetRequester(url)
+
+response_body = requester.get_response_body()
+if response_body:
+    print("Response Body:")
+    print(response_body)
+
+json_data = requester.load_json()
+if json_data:
+    print("JSON Data:")
+    print(json_data)
